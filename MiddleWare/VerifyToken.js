28743376e831af486c0c06d97f5994657bdaf4da
Denzil-l <Denzil.l@yandex.ru>
2023-09-21 19:04:04 +0300

    server

--- MiddleWare/VerifyToken.js
+++ MiddleWare/VerifyToken.js
@@ -7,7 +7,9 @@ export const verifyToken = (req,res, next) => {
     const accessToken = req.body.accessToken
     console.log('I am here in Verify')
     console.log(req.body)
-    if(!accessToken) return res.status(401).json({message: 'unathorized'})
+    if(!accessToken){
+        return res.status(401).json({message: 'unathorized'})
+    }
 
     jwt.verify(accessToken,process.env.ACCESS_TOKEN_SECRET_KEY,(err, decoded) => {
         if(err){
