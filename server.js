e5b4c0dc96d2550b7f30d4deade8c6f7ccc6fef9
Denzil-l <Denzil.l@yandex.ru>
2023-09-21 16:16:22 +0300

    servermanishr

--- server.js
+++ server.js
@@ -19,13 +19,26 @@ app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.status(200).send('text')
-    console.log("Я выполнил запрос")
-    const userAgent = req.headers['user-agent']
+app.use((req, res, next) => {
+    const userAgent = req.get('User-Agent');
     console.log(userAgent)
-})
-
+    if (userAgent.includes('MobileAppIdentifier')) {
+      // Это мобильное приложение
+      req.isMobileApp = true;
+    } else {
+      // Это браузер
+      req.isMobileApp = false;
+    }
+    next();
+  });
+  
+  app.get('/', (req, res) => {
+    if (req.isMobileApp) {
+        res.status(200).json({message: 'Это запрос от мобильного приложения.'});
+    } else {
+        res.status(200).json({message: 'Это запрос от браузера.'});
+    }
+  });
 app.listen(process.env.PORT, () => console.log(`Server is working in the PORT: ${process.env.PORT}`))
 
 app.use('/auth', userRouter)
